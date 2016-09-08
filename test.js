19fc64c10287a9678c5fffe5d969203fdeb1ecd6
Julian 'Andrakis' Thatcher <daedalus.raistlin@gmail.com>
2016-09-09 00:10:41 +1000

    Clean up

--- test.js
+++ test.js
@@ -7,5 +7,11 @@ var lithp = new Lithp();
 (require('./lib/builtins')).setup(lithp);
 //debug(lithp.functions);
 //lithp.run(less_simple_test_code(lithp));
-lithp.run(samples.fac_test_code (lithp));
+var sample;
+// Select your desired sample here
+//sample = samples.simple_test_code(lithp);
+//sample = samples.simple_test_code2(lithp);
+//sample = samples.less_simple_test_code(lithp);
+sample = samples.fac_test_code(lithp)
+lithp.run(sample);
 
