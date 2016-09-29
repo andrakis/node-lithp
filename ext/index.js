06920606a521b3bb0133c8cbd9aaa3c8e291e604
Julian Thatcher <daedalus.raistlin@gmail.com>
2016-09-30 00:48:07 +1000

    Can now use custom platform libraries
    Platform libraries allow one to extend the interpreter by adding new
    runtime builtin functions in JavaScript. This allows projects that use Lithp
    to provide necessary runtime functions.
    
    The platform/1 builtin now supports platform libraries.
    An example platform library is provided in ext/index.js.
    
    When platform/1 is called, it searches for the given name + /index.js.
    This means you can provide a string or an atom to the platform/1 function.

---
+++ ext/index.js
@@ -0,0 +1,42 @@
+/**
+ * Example extension library for Lithp.
+ *
+ * Adds new builtins to Lithp.
+ */
+var util = require('util'),
+	inspect = util.inspect;
+// TODO: Ensure correct path to lithp/index.js.
+//       Projects using Lithp as a Node module should adjust this path.
+var lithp = require(__dirname + '/../index'),
+	Lithp = lithp.Lithp,
+	debug = lithp.debug,
+	types = lithp.Types,
+	OpChain = types.OpChain,
+	Atom = types.Atom,
+	GetAtoms = types.GetAtoms,
+	FunctionCall = types.FunctionCall,
+	FunctionReentry = types.FunctionReentry,
+	FunctionDefinition = types.FunctionDefinition,
+	FunctionDefinitionNative = types.FunctionDefinitionNative,
+	AnonymousFunction = types.AnonymousFunction,
+	LiteralValue = types.LiteralValue,
+	VariableReference = types.VariableReference,
+	Tuple = types.Tuple;
+
+var builtins = {};
+function builtin (name, params, body) {
+	builtins[name] = {params: params, body: body};
+}
+
+// See lib/builtins.js for more examples.
+builtin("count-params/*", [], Params => Params.length);
+
+exports.setup = function(lithp) {
+	var count = 0;
+	for(var k in builtins) {
+		lithp.builtin(k, builtins[k].params, builtins[k].body);
+		count++;
+	}
+	return count;
+};
+
